a36d227ec9935ced4b618b1d6135011cc35ef6a5
Maciej Koch <maciej.koch@vattenfall.com>
2018-09-02 16:46:21 +0200

    Added deleting item

--- src/app/topic/topic.page.ts
+++ src/app/topic/topic.page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { finalize, tap, catchError } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { Topic } from '../topics-list/topic.modet';
 import { TopicsService } from '../topics-list/topics.service';
-import { AlertController } from '@ionic/angular';
+import { AlertController, PopoverController } from '@ionic/angular';
 import { TopicItem } from '../topics-list/topic-item.model';
 import { LinkPreviewService } from './link-preview.service';
+import { guid } from '../helpers/guid-generator.helper';
 
 @Component({
   selector: 'app-topic',
@@ -15,8 +17,9 @@ import { LinkPreviewService } from './link-preview.service';
 export class TopicPage {
 
   item: Topic;
+  edit: boolean = true;
 
-  constructor(private route: ActivatedRoute, private alertController: AlertController, private topicsService: TopicsService, private linkPreviewService: LinkPreviewService) {}
+  constructor(private route: ActivatedRoute, private alertController: AlertController, private popoverController: PopoverController, private topicsService: TopicsService, private linkPreviewService: LinkPreviewService) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -26,7 +29,11 @@ export class TopicPage {
     });
   }
 
-  async presentNewUrlAlert() {
+  toggleEdit(): void {
+    this.edit = !this.edit;
+  }
+
+  async presentNewUrlAlert(): Promise<void> {
     const alert = await this.alertController.create({
       header: 'New url',
       inputs: [{
@@ -36,7 +43,7 @@ export class TopicPage {
       }],
       buttons: [{
         text: 'Ok',
-        handler: data => this.createTopicItem(data)
+        handler: data => this.addTopicItem(this.item, data)
       }, {
         text: 'Cancel',
         role: 'cancel'
@@ -46,12 +53,40 @@ export class TopicPage {
     await alert.present();
   }
 
-  private createTopicItem(topicItem: TopicItem) {
-    this.linkPreviewService.getLinkPreview(topicItem.url).subscribe((preview: TopicItem) => {
-      const topicItems = this.item.items ? [...this.item.items, preview ] : [preview]
-      const updatedItem = {...this.item, items: topicItems};
-      this.topicsService.updateItem(updatedItem);
+  async delete(topicItem: TopicItem): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Are you sure?',
+      buttons: [{
+        text: 'Ok',
+        handler: () => this.removeItemAndSave(this.item, topicItem)
+      }, {
+        text: 'Cancel',
+        role: 'cancel'
+      }]
     })
 
+    await alert.present();
   }
+
+  private addTopicItem(item: Topic, topicItem: TopicItem) {
+    this.linkPreviewService.getLinkPreview(topicItem.url)
+    .subscribe(
+      (preview: TopicItem) => this.addItemAndSave(item, preview),
+      () => this.addItemAndSave(item, topicItem)
+    )
+  }
+
+  private addItemAndSave(item: Topic, topicItem: TopicItem): Observable<Topic> {
+    const newItem = {...topicItem, id: guid() };
+    const topicItems = this.item.items ? [...this.item.items, newItem ] : [newItem]
+    const updatedItem = {...item, items: topicItems};
+    return this.topicsService.updateItem(updatedItem);
+  }
+
+  private removeItemAndSave(item: Topic, topicItem: TopicItem): Observable<Topic> {
+    const topicItems = this.item.items.filter(item => item.id !== topicItem.id)
+    const updatedItem = {...item, items: topicItems};
+    return this.topicsService.updateItem(updatedItem);
+  }
+
 }
--- src/app/topics-list/topic-item.model.ts
+++ src/app/topics-list/topic-item.model.ts
@@ -1,4 +1,5 @@
 export interface TopicItem {
+  id: string,
   url: string,
   title: string,
   image: string,
